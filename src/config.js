9ce7be814d96c24db55c1dbea063d81cde1122ea
James Kong <fishkingsin@gmail.com>
2018-10-23 23:54:36 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import Config from 'react-native-config';
+
+export const config = {
+	urls: {
+		api: Config.API_URL,
+	},
+};
---
+++ src/lib/sdk/index.js
@@ -0,0 +1,37 @@
+import { Platform } from 'react-native';
+import { config } from '~/config';
+
+export const callApi = (method, url, callCfg = {}) => {
+	const {
+        baseUrl = config.urls.api,
+        headers,
+		...otherCfg
+	} = callCfg;
+	const reqCfg = {
+		method,
+		headers: {
+			'content-type': url.startsWith('/d/') ? 'application/x-www-form-urlencoded' : 'application/json',
+			...headers,
+		},
+		...otherCfg,
+	};
+	if (token && typeof token === 'string') {
+		reqCfg.headers.Authorization = `Bearer ${token}`;
+	}
+	if (data !== null) {
+		reqCfg.body = handleRequestData(reqCfg.headers['content-type'], data);
+	}
+	const query = (params !== null) ? `?${jsonToURLEncoded(params)}` : '';
+	// console.log('here callApi config', `${baseUrl}${url}${query}`, reqCfg);
+	return fetch(`${baseUrl}${url}${query}`, reqCfg)
+		.then(async (res) => {
+			// console.log('here callApi', url, ' result', res);
+			const responseText = await res.text();
+			// console.log('here callApi', url, ' responseText', responseText);
+			const responseJson = responseText ? JSON.parse(responseText) : '';
+			if (!res.ok) {
+				return Promise.reject(responseJson);
+			}
+			return responseJson;
+		});
+};
