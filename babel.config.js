a64ffe105cf9b902f84ff699751cf73bc09bdb76
James Kong <james.kong@nixplay.com>
2019-05-08 17:33:01 +0800

    Update project

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	"presets": [
+		"module:metro-react-native-babel-preset"
+	],
+	"plugins": [
+		[
+			"module-resolver",
+			{
+				"cwd": "babelrc",
+				"extensions": [
+					".js",
+					".ios.js",
+					".android.js"
+				],
+				"alias": {
+					"~": "./src",
+					"~comp": "./src/components",
+					"~cont": "./src/containers",
+					"helper": "./__tests__/testHelpers"
+				}
+			}
+		],
+		[
+			"@babel/plugin-transform-spread",
+			{
+				"loose": true
+			}
+		],
+		"relay",
+    	"jest-hoist",
+		"import-glob"
+	]
+}
